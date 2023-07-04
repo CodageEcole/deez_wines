1e2e7399112abd2138e60c9f7ec7c75fcaeceaa5
e1754709 <e1754709@cmaisonneuve.qc.ca>
2023-07-04 12:02:29 -0400

    modification pour acceder a la bd, non au JSON

--- app/Http/Controllers/Sprint0Controller.php
+++ app/Http/Controllers/Sprint0Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bouteille;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -11,7 +12,7 @@ class Sprint0Controller extends Controller
 
     public function demoListe() {
 
-        $bouteilles = Storage::json("bouteillestest3.json");
+        $bouteilles = Bouteille::all();
         $codesTraites = 0;
         $erreur = [];
 
