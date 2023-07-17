00363f45a09edd4f2b8fecabdf4c489065767874
Philippe Malo <malo.philippe2021@gmail.com>
2023-07-17 15:15:20 -0400

    début layout/css celliers, changements au modèle pour récupérer la quantité de bouteilles de chaque couleur.

--- resources/views/celliers/index.blade.php
+++ resources/views/celliers/index.blade.php
@@ -1,17 +1,27 @@
 @extends('layout.app')
 @section('title', 'Vos Celliers')
+@push('styles')
+    <link href="{{ asset('css/celliers.css') }}" rel="stylesheet">
+@endpush
 @section('content')
-<main>
-    <div>
-        @foreach($celliers as $cellier)
+<main class="celliers">
+    @foreach($celliers as $cellier)
+        <div>
             <div class="cellier">
-                <a href="{{ route('celliers.show', $cellier->id) }}">{{ $cellier->nom }}</a>
-                <div>
-                    <span>Nombres de bouteilles : {{ $cellier->quantite_bouteilles }}</span>
-                </div>
+                <a href="{{ route('celliers.show', $cellier->id) }}">{{ $cellier->nom }}
+                    <div>
+                        <span>Bouteilles : {{ $cellier->quantite_bouteilles }}</span>
+                        <hr>
+                        @if($cellier->quantite_bouteilles > 0)
+                        <span>Rouge : {{ $cellier->quantiteBouteillesRouges() ?? 0 }} Rosé : {{ $cellier->quantiteBouteillesRoses() ?? 0 }} Blanc : {{ $cellier->quantiteBouteillesBlanches() ?? 0 }}</span>
+                        @endif
+                    </div>
+                </a>
             </div>
-        @endforeach
+        </div>
+    @endforeach
+    <div>
+        <a href="{{ route('celliers.create') }}"><img src="{{ asset('icons/plus_icon.svg') }}" alt="Ajouter">Ajouter</a>
     </div>
-    <a href="{{ route('celliers.create') }}">Ajouter un cellier</a>
 </main>
 @endsection
