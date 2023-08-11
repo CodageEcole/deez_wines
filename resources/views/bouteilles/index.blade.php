105f8aa1518377cf6217309731deb826f850520e
Philippe Malo <malo.philippe2021@gmail.com>
2023-08-11 00:02:12 -0400

    changements majeurs à la recherche, tous les champs fonctionnent, ajustement fabrication de l'url en javascript, ajout du prix temporairement dans la carte, changement des valeurs des couleurs dans index.blade

--- resources/views/bouteilles/index.blade.php
+++ resources/views/bouteilles/index.blade.php
@@ -32,11 +32,11 @@
     @csrf
     <div id="couleurs" class="filtre">
         <label for="filtre-rouge">@lang('messages.red')</label>
-        <input type="checkbox" name="filtre-rouge" id="filtre-rouge" value="rouge">
+        <input type="checkbox" name="filtre-rouge" id="filtre-rouge" value="Rouge">
         <label for="filtre-blanc">@lang('messages.white')</label>
-        <input type="checkbox" name="filtre-blanc" id="filtre-blanc" value="blanc">
+        <input type="checkbox" name="filtre-blanc" id="filtre-blanc" value="Blanc">
         <label for="filtre-rose">@lang('messages.rose')</label>
-        <input type="checkbox" name="filtre-rose" id="filtre-rose" value="rose">
+        <input type="checkbox" name="filtre-rose" id="filtre-rose" value="Rosé">
         <label for="filtre-orange">@lang('messages.orange')</label>
         <input type="checkbox" name="filtre-orange" id="filtre-orange" value="orange">
     </div>
