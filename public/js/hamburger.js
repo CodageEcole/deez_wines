90923a60bd819a1fd933522768617901ccdbd496
Philippe Malo <malo.philippe2021@gmail.com>
2023-08-16 00:09:11 -0400

    correctifs pour avoir une option de fermeture additionnelle du menuFiltre et menuMobile sur petits écrans

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -7,42 +7,55 @@ const overlayGreyFiltre = document.querySelector(".overlay-grey-filtre");
 // menu filtre
 const filtreBouton = document.querySelector(".filtres-trigger");
 const menuFiltre = document.querySelector(".filtre-nav");
+const menuIcon = menuBouton.querySelector("img");
 
 // fonction pour shadows (parce qu'il y a 3 possibilites, pour le menu option, filtre et sort)
 function toggleShadows(element, target, classActive) {
     element.addEventListener("click", function() {
         target.classList.toggle(classActive);
         element.classList.toggle("show");
+        menuIcon.src = window.location.origin + "/icons/more_icon.svg";
     });
 }
 
 window.onload = function() {
-
-    // fonction options supplementaires
+    // Fonction pour le bouton de menu
     menuBouton.addEventListener("click", function() {
-        menuMobile.classList.toggle("is-active");
-        overlayGrey.classList.toggle("show");
-
-        // Pour empecher les overlaps de menus
-        if(menuFiltre) {
+        if (menuFiltre && menuFiltre.classList.contains("is-active-filtre")) {
             menuFiltre.classList.remove("is-active-filtre");
-        }
-
-        if(overlayGreyFiltre){
             overlayGreyFiltre.classList.remove("show");
+            menuIcon.src = window.location.origin + "/icons/more_icon.svg";
+        } else if (menuMobile.classList.contains("is-active")) {
+            menuMobile.classList.remove("is-active");
+            overlayGrey.classList.remove("show");
+            menuIcon.src = window.location.origin + "/icons/more_icon.svg";
+        } else {
+            menuMobile.classList.toggle("is-active");
+            overlayGrey.classList.toggle("show");
+            menuIcon.src = window.location.origin + "/icons/right_arrow.svg";
         }
+    });
 
-    })
-
-    // fonction filtre
-    if(filtreBouton) {
+    // Fonction pour le menu filtre
+    if (filtreBouton) {
         filtreBouton.addEventListener("click", function() {
+            if (menuMobile.classList.contains("is-active")) {
+                menuMobile.classList.remove("is-active");
+                overlayGrey.classList.remove("show");
+                menuIcon.src = window.location.origin + "/icons/more_icon.svg";
+            }
             menuFiltre.classList.toggle("is-active-filtre");
             overlayGreyFiltre.classList.toggle("show");
-        })
+
+            if (menuFiltre.classList.contains("is-active-filtre")) {
+                menuIcon.src = window.location.origin + "/icons/left_arrow.svg";
+            } else {
+                menuIcon.src = window.location.origin + "/icons/more_icon.svg";
+            }
+        });
     }
 
-    // fonctions pour les shadows
+    // Fonctions pour les ombres
     toggleShadows(overlayGrey, menuMobile, "is-active");
-    toggleShadows(overlayGreyFiltre, menuFiltre, "is-active-filtre")
+    toggleShadows(overlayGreyFiltre, menuFiltre, "is-active-filtre");
 }
